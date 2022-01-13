f6a328d3f333c6a15a8fc294881ffd27923f3780
ensarkoras <ensarkoras@gmail.com>
2022-01-13 12:33:03 +0300

    second

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about-page">
+      <h2>About page</h2>
+    </div>
+</template>
+
+<script>
+export default {
+name: "about"
+}
+</script>
+
+<style scoped>
+
+</style>
